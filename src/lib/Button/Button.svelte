e912c10b2a09a46894942253016d623dfc87bf03
Michael Winberry <mike@defenseunicorns.com>
2022-10-17 12:05:45 -0700

    Extract ButtonProps type from Button.svelte into Button.types. Remove explicit declaration of href prop in favor of 15900restProps declaration. Open up inherited props and events using the box component.

--- src/lib/Button/Button.svelte
+++ src/lib/Button/Button.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-	import type { ButtonVariant, ButtonColor, ButtonShape } from './Button.types';
 	import { onMount } from 'svelte';
+	import Box from '$lib/Box/box.svelte';
 	import { MDCRipple } from '@material/ripple';
+	import { current_component } from 'svelte/internal';
+	import type { ButtonVariant, ButtonColor, ButtonShape, ButtonProps } from './Button.types';
 
 	// Props
-	export let variant: ButtonVariant = 'text';
 	export let color: ButtonColor = 'primary';
 	export let shape: ButtonShape = 'squared';
-	export let href = '';
+	export let variant: ButtonVariant = 'text';
 
-	interface $$Props extends svelte.JSX.HTMLAttributes<HTMLButtonElement | HTMLAnchorElement> {
-		color?: ButtonColor;
-		shape?: ButtonShape;
-		variant?: ButtonVariant;
-		href?: string;
-	}
+	type $$Props = ButtonProps;
 
 	// Local Vars
 	let buttonElement: HTMLButtonElement | HTMLAnchorElement;
@@ -26,26 +22,33 @@
 		}
 	});
 
-	$: element = href ? 'a' : 'button';
+	function getIconClass(): string {
+		let classes: string[] = [];
+		if ($$slots.leadingIcon) {
+			classes.push('mdc-button--icon-leading');
+		}
+		if ($$slots.trailingIcon) {
+			classes.push('mdc-button--icon-leading');
+		}
+		return classes.join(' ');
+	}
 </script>
 
-<svelte:element
-	this={element}
-	bind:this={buttonElement}
-	on:submit
-	on:click
-	{href}
+<Box
 	{...$$restProps}
-	class={`unicorn-button mdc-button ${variant} ${color} ${shape} ${$$restProps.class}`}
-	class:mdc-button--icon-leading={$$slots.leadingIcon}
-	class:mdc-button--icon-trailing={$$slots.trailingIcon}
+	bind:ref={buttonElement}
+	eventComponent={current_component}
+	element={$$restProps.href ? 'a' : 'button'}
+	class={`button mdc-button ${getIconClass()} ${variant} ${color} ${shape} ${
+		$$restProps.class || ''
+	}`}
 >
-	<span class="mdc-button__ripple" />
-	<span class="mdc-button__focus-ring" />
+	<span class="button-ripple mdc-button__ripple" />
+	<span class="button-focus-ring mdc-button__focus-ring" />
 	<slot name="leadingIcon" />
-	<span class="mdc-button__label"><slot>{`${variant} button`}</slot></span>
+	<span class="button-label mdc-button__label"><slot>{`${variant} button`}</slot></span>
 	<slot name="trailingIcon" />
-</svelte:element>
+</Box>
 
 <style lang="scss" global>
 	@import '@material/button/mdc-button';
