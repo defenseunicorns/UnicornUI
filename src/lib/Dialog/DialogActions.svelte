9d076f33debb43892c30fd43bbdaadd9f6cd3c7d
Michael Winberry <mike@defenseunicorns.com>
2022-11-08 06:46:16 -0800

    Update DialogActions component to extend Box props.

--- src/lib/Dialog/DialogActions.svelte
+++ src/lib/Dialog/DialogActions.svelte
@@ -1,3 +1,20 @@
-<div class="mdc-dialog__actions">
+<script lang="ts">
+	import Box from '$lib/Box/box.svelte';
+	import type { BoxProps } from '$lib/Box/box.types';
+	import { current_component } from 'svelte/internal';
+
+	type T = $$Generic<EventTarget>;
+	// Props
+	export let element = 'div';
+
+	type $$Props = BoxProps<T>;
+</script>
+
+<Box
+	{element}
+	eventComponent={current_component}
+	{...$$restProps}
+	class={`mdc-dialog__actions ${$$restProps.class || ''}`}
+>
 	<slot />
-</div>
+</Box>
