b5dbc78944840c31b34c2c618930a08bf88736b7
Michael Winberry <mike@defenseunicorns.com>
2022-11-09 06:42:25 -0800

    Remove index.d.ts: moved exported types back to index.ts to prevent interference with sveltekit auto-generated index.d.ts. Validated that all types and components are properly exported. Remove shared/types/LinkProps.types.ts: as they are no longer applicable since the move to using HTML component props and events. Update type references within the website.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -26,3 +26,23 @@ export {
 	DialogTitle,
 	DialogActions
 };
+
+// Re-export your types here.
+export type {
+	ButtonVariant,
+	ButtonColor,
+	ButtonShape,
+	ButtonProps,
+	ButtonIconProps
+} from './Button/Button.types';
+export type {
+	TypographyVariant,
+	TypographyElement,
+	TypographyElements,
+	TypographyProps
+} from './Typography/typography.types';
+export type { BoxProps } from './Box/box.types';
+export type { AccordionProps } from './Accordion/accordion.types';
+export type { DialogActionsProps, DialogTitleProps } from './Dialog/Dialog.types';
+export type { IconButtonColor, IconButtonProps } from './IconButton/IconButton.types';
+export type { StepVariant, StepOrientation, StepProps } from './Stepper/stepper.types';
