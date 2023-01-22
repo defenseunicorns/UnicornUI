6bd495b90cc5386614e4bcd1a624cb3c1391db8c
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-01-21 22:13:25 -0700

    website.spec.ts format changes.

--- website/playwright-tests/website.spec.ts
+++ website/playwright-tests/website.spec.ts
@@ -1,21 +1,20 @@
-import { test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.describe('unicorn ui website e2e tests', () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto("http://localhost:4173");
-    })
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4173');
+  });
 
-    test('load home page', async ({page}) => {
-        expect(page.url()).toEqual("http://localhost:4173/")
-        expect(page.getByText('Unicorn UI')).toBeTruthy();
-        expect(page.getByText('Installation')).toBeTruthy();
-    })
+  test('load home page', async ({ page }) => {
+    expect(page.url()).toEqual('http://localhost:4173/');
+    expect(page.getByText('Unicorn UI')).toBeTruthy();
+    expect(page.getByText('Installation')).toBeTruthy();
+  });
 
-    test('nav to /theme', async ({page}) => {
-        const pageNav =  page.waitForNavigation({ url: '**/theme' });
-        page.getByText(/^theme$/i).click();
-        await pageNav;
-        expect(page.url()).toContain('/theme');
-    })
-})
-
+  test('nav to /theme', async ({ page }) => {
+    const pageNav = page.waitForNavigation({ url: '**/theme' });
+    page.getByText(/^theme$/i).click();
+    await pageNav;
+    expect(page.url()).toContain('/theme');
+  });
+});
