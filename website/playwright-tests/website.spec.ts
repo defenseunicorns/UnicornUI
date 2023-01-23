b81ffb2ab800e81d344529c2c399b90be838628a
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-01-23 14:50:46 -0700

    #87 -- revised playwright tests to systematically find links, click on them, and check url path.

--- website/playwright-tests/website.spec.ts
+++ website/playwright-tests/website.spec.ts
@@ -1,4 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
+
+const testLink = async (linkLocator: Locator, page: Page): Promise<void> => {
+  const linkUrl = await linkLocator.getAttribute('href');
+  if (linkUrl!.includes('https://')) return;
+  const pageNav = page.waitForNavigation({ url: `**${linkUrl}` });
+  linkLocator.click();
+  await pageNav;
+  expect(page.url()).toContain(linkUrl);
+};
+
+const resetToHome = async (page: Page) => {
+  const pageNav = page.waitForNavigation({ url: `**/` });
+  page.getByText(/home/i).click();
+  await pageNav;
+};
 
 test.describe('unicorn ui website e2e tests', () => {
   test.beforeEach(async ({ page }) => {
@@ -9,12 +24,13 @@ test.describe('unicorn ui website e2e tests', () => {
     expect(page.url()).toEqual('http://localhost:4173/');
     expect(page.getByText('Unicorn UI')).toBeTruthy();
     expect(page.getByText('Installation')).toBeTruthy();
+    expect(page.getByText(/github/i)).toBeTruthy();
   });
 
-  test('nav to /theme', async ({ page }) => {
-    const pageNav = page.waitForNavigation({ url: '**/theme' });
-    page.getByText(/^theme$/i).click();
-    await pageNav;
-    expect(page.url()).toContain('/theme');
+  test('test links', async ({ page }) => {
+    for (const link of await page.getByRole('link').all()) {
+      await testLink(link, page);
+      await resetToHome(page);
+    }
   });
 });
