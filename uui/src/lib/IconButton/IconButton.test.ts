0d654b2876add08969559c5d5196e4bb1a21f0a3
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-02-20 10:21:31 -0700

    #107 -- removed more manual vitest / jsdom version of IconButton test for now since @testing-library/svelte gives more brevity and readability.

--- uui/src/lib/IconButton/IconButton.test.ts
+++ uui/src/lib/IconButton/IconButton.test.ts
@@ -2,30 +2,9 @@ import IconButton from './IconButton.svelte';
 import { render, screen } from '@testing-library/svelte';
 import '@testing-library/jest-dom';
 
-let host: HTMLElement;
-
-describe('vitest and jsdom manually', () => {
-  beforeEach(() => {
-    host = document.createElement('div');
-    host.setAttribute('id', 'host');
-    document.body.appendChild(host);
-  });
-
-  afterEach(() => {
-    host.remove();
-  });
-
-  it('it is toggled on', () => {
-    new IconButton({ target: host, props: { toggleable: true, toggled: true } });
-
-    expect(host.querySelector('button')?.getAttribute('class')).toContain('mdc-icon-button--on');
-  });
-});
-
-describe('using @testing-library/svelte', () => {
-  it('renders icon button', () => {
+describe('IconButton states', () => {
+  it('renders icon button as on based on props', () => {
     render(IconButton, { props: { toggleable: true, toggled: true } });
-
     expect(screen.getByRole('button')).toHaveClass('mdc-icon-button--on');
   });
 });
