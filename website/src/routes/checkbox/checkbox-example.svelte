501c24dd1e1bd06a4eb33cd936340688f8f46390
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-04-11 13:36:53 -0600

    #149 -- refactored checkbox examples into loop and added examples without labels.

--- website/src/routes/checkbox/checkbox-example.svelte
+++ website/src/routes/checkbox/checkbox-example.svelte
@@ -4,28 +4,75 @@
   let formFieldsSSX = {
     $self: { display: 'flex', 'align-items': 'center', margin: '.5rem' }
   };
+
+  const checkboxes = [
+    { label: 'Inactive', checked: false, indeteriminate: false, id: 'checkbox-1', disabled: false },
+    { label: 'Checked', checked: true, indeteriminate: false, id: 'checkbox-2', disabled: false },
+    {
+      label: 'Indeteriminate',
+      checked: false,
+      indeteriminate: true,
+      id: 'checkbox-3',
+      disabled: false
+    },
+    { label: 'Disabled', checked: false, indeteriminate: false, id: 'checkbox-4', disabled: true },
+    {
+      label: 'Disabled Checked',
+      checked: false,
+      indeteriminate: false,
+      id: 'checkbox-5',
+      disabled: true
+    },
+    {
+      label: 'Stroke Color',
+      checked: true,
+      indeteriminate: false,
+      id: 'checkbox-6',
+      disabled: false,
+      color: 'red'
+    },
+    {
+      label: 'Background Color',
+      checked: true,
+      indeteriminate: false,
+      id: 'checkbox-7',
+      disabled: false,
+      backgroundColor: 'red'
+    }
+  ];
 </script>
 
-<Paper elevation={16} ssx={{ $self: { padding: '1rem' } }}>
+<Paper
+  elevation={16}
+  ssx={{ $self: { padding: '1rem', display: 'flex', 'justify-content': 'space-around' } }}
+>
   <form>
-    <Box ssx={formFieldsSSX}>
-      <Checkbox id="checkbox-1" label="Enabled" />
-    </Box>
-    <Box ssx={formFieldsSSX}>
-      <Checkbox checked id="checkbox-2" label="Checked" />
-    </Box>
-    <Box ssx={formFieldsSSX}>
-      <Checkbox disabled label="Disabled" id="checkbox-3" />
-    </Box>
-    <Box ssx={formFieldsSSX}>
-      <Checkbox checked disabled label="Disabled Selected" id="checkbox-4" />
-    </Box>
-
-    <Box ssx={formFieldsSSX}>
-      <Checkbox id="checkbox-5" label="Stroke Color" color="red" checked />
-    </Box>
-    <Box ssx={formFieldsSSX}>
-      <Checkbox id="checkbox-6" label="Container Color" backgroundColor="red" checked />
-    </Box>
+    {#each checkboxes as checkbox}
+      <Box ssx={formFieldsSSX}>
+        <Checkbox
+          id={checkbox.id}
+          label={checkbox.label}
+          disabled={checkbox.disabled}
+          checked={checkbox.checked}
+          indeterminate={checkbox.indeteriminate}
+          color={checkbox.color}
+          backgroundColor={checkbox.backgroundColor}
+        />
+      </Box>
+    {/each}
+  </form>
+  <form>
+    {#each checkboxes as checkbox}
+      <Box ssx={formFieldsSSX}>
+        <Checkbox
+          id={checkbox.id}
+          disabled={checkbox.disabled}
+          checked={checkbox.checked}
+          indeterminate={checkbox.indeteriminate}
+          color={checkbox.color}
+          backgroundColor={checkbox.backgroundColor}
+        />
+      </Box>
+    {/each}
   </form>
 </Paper>
