f3ae8741a094becdbc1fca27762786f60e8f91ae
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-05-29 15:42:26 -0600

    #220 -- removed nested list headers from website nav drawer; refactored appStatesStore to only track currentRoute now.

--- website/src/lib/NavigationDrawer.svelte
+++ website/src/lib/NavigationDrawer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Box, Drawer, ListItem, ListItemAdornment, Typography } from '@uui';
+  import { Box, Drawer, ListItem, ListSubHeader, Typography } from '@uui';
   import { onMount } from 'svelte';
   import { appStatesStore, updateAppStates } from './stores/nav-drawer-state-store';
 
@@ -8,8 +8,6 @@
 
   // Local vars
   let selectedRoute = '';
-  let themingListOpen = false;
-  let componentListOpen = false;
 
   const themeRoutestList = ['theme', 'breakpoints', 'scoped-styles', 'typography', 'box', 'paper'];
   const componentRoutesList = [
@@ -31,8 +29,6 @@
   // Functions
   onMount(() => {
     appStatesStore.subscribe((states) => {
-      themingListOpen = states.listStates.theming;
-      componentListOpen = states.listStates.components;
       selectedRoute = states.currentRoute;
     });
   });
@@ -48,72 +44,41 @@
     words = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
     return words.join(' ');
   }
-
-  function updateDrawerStates(list: string) {
-    appStatesStore.update((states) => {
-      if (list === 'theming') {
-        return updateAppStates({
-          ...states,
-          listStates: { ...states.listStates, theming: !themingListOpen }
-        });
-      } else {
-        return updateAppStates({
-          ...states,
-          listStates: { ...states.listStates, components: !componentListOpen }
-        });
-      }
-    });
-  }
 </script>
 
 <Drawer elevation={2} open={isDrawerOpen} ssx={{ $self: { height: 'calc(100vh - 56px)' } }}>
   <Box>
-    <ListItem selected={themingListOpen} on:click={() => updateDrawerStates('theming')}>
-      <Typography>Theming</Typography>
-      <ListItemAdornment slot="trailing" class="material-symbols-outlined">
-        {themingListOpen ? 'expand_less' : 'expand_more'}
-      </ListItemAdornment>
-      <svelte:fragment slot="sublist">
-        {#each themeRoutestList as route}
-          <ListItem
-            selected={selectedRoute === route}
-            element="a"
-            href="/{route}"
-            on:click={(e) => {
-              e.stopPropagation();
-              setSelectedRoute(route);
-            }}
-          >
-            <Typography>
-              {transformRoute(route)}
-            </Typography>
-          </ListItem>
-        {/each}
-      </svelte:fragment>
-    </ListItem>
-
-    <ListItem selected={componentListOpen} on:click={() => updateDrawerStates('components')}>
-      <Typography>Components</Typography>
-      <ListItemAdornment slot="trailing" class="material-symbols-outlined">
-        {componentListOpen ? 'expand_less' : 'expand_more'}
-      </ListItemAdornment>
-      <svelte:fragment slot="sublist">
-        {#each componentRoutesList as route}
-          <ListItem
-            selected={selectedRoute === route}
-            element="a"
-            href="/{route}"
-            on:click={(e) => {
-              e.stopPropagation();
-              setSelectedRoute(route);
-            }}
-          >
-            <Typography>
-              {transformRoute(route)}
-            </Typography>
-          </ListItem>
-        {/each}
-      </svelte:fragment>
-    </ListItem>
+    <ListSubHeader>Theming</ListSubHeader>
+    {#each themeRoutestList as route}
+      <ListItem
+        selected={selectedRoute === route}
+        element="a"
+        href="/{route}"
+        on:click={(e) => {
+          e.stopPropagation();
+          setSelectedRoute(route);
+        }}
+      >
+        <Typography>
+          {transformRoute(route)}
+        </Typography>
+      </ListItem>
+    {/each}
+    <ListSubHeader>Components</ListSubHeader>
+    {#each componentRoutesList as route}
+      <ListItem
+        selected={selectedRoute === route}
+        element="a"
+        href="/{route}"
+        on:click={(e) => {
+          e.stopPropagation();
+          setSelectedRoute(route);
+        }}
+      >
+        <Typography>
+          {transformRoute(route)}
+        </Typography>
+      </ListItem>
+    {/each}
   </Box>
 </Drawer>
--- website/src/lib/stores/nav-drawer-state-store.ts
+++ website/src/lib/stores/nav-drawer-state-store.ts
@@ -2,18 +2,13 @@ import { writable, type Writable } from 'svelte/store';
 import { browser } from '$app/environment';
 
 type AppStates = {
-  listStates: {
-    theming: boolean;
-    components: boolean;
-  };
   currentRoute: string;
 };
 
 export let appStatesStore: Writable<AppStates>;
 
 if (browser) {
-  console.log('getting appStates');
-  let states: AppStates = { listStates: { theming: true, components: true }, currentRoute: '' };
+  let states: AppStates = { currentRoute: '' };
 
   if (localStorage.getItem('appStates')) {
     states = JSON.parse(localStorage.getItem('appStates') || JSON.stringify(states));
