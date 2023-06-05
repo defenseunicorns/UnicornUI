edb4a7b1f31f40afb022e2caeadce7d9bcb36dad
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-06-05 16:28:09 -0600

    #217 -- moved Textfield website examples to separate files.

--- website/src/routes/text-field/+page.svelte
+++ website/src/routes/text-field/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-  import { TextField, TextFieldIcon } from '@uui';
+  import { TextField } from '@uui';
   import Variant from '$lib/Variant.svelte';
-  import { AccordionGroup, Accordion, Typography, Box, Button } from '@uui';
-
-  let formValue = '';
+  import { AccordionGroup, Accordion, Typography, Box } from '@uui';
+  import VALIDATION_EXP from './validation-example.svelte?raw';
+  import VariantExample from '$lib/VariantExample.svelte';
+  import ValidationExample from './validation-example.svelte';
+  import ThemingExample from './theming-example.svelte';
+  import THEMING_EXP from './theming-example.svelte?raw';
+  import SlotsExample from './slots-example.svelte';
+  import SLOTS_EXP from './slots-example.svelte?raw';
+  import PropsExample from './props-example.svelte';
+  import PROPS_EXP from './props-example.svelte?raw';
 </script>
 
 <Variant
@@ -16,12 +23,7 @@
   </div>
   <div class="labeled-example">
     <Typography variant="subtitle1">Filled:</Typography>
-    <TextField
-      label="Label"
-      variant="filled"
-      color="primary"
-      ssx={{ width: '100rem', backgroundColor: 'red' }}
-    />
+    <TextField label="Label" variant="filled" color="primary" />
   </div>
   <AccordionGroup>
     <Accordion isOpen={true}>
@@ -31,190 +33,26 @@
           The Text Field component accepts native HTMLInput props as well as several Unicorn UI
           defined ones implemented by the TextFieldProps interface.
         </Typography>
-        <Box class="text-field-example">
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Required:</Typography>
-            <TextField required label="Label" variant="outlined" color="primary" />
-          </div>
 
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Helper Text:</Typography>
-            <TextField
-              label="Label"
-              variant="outlined"
-              color="primary"
-              helperText="supporting text"
-            />
-          </div>
-
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Character Counter:</Typography>
-            <TextField
-              label="Label"
-              variant="outlined"
-              color="primary"
-              characterCounter
-              inputProps={{ maxlength: 60 }}
-              maxlength={60}
-            />
-          </div>
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Disabled:</Typography>
-            <TextField label="Label" variant="outlined" color="primary" value="Value" disabled />
-          </div>
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Error:</Typography>
-            <TextField error label="Label" variant="outlined" color="primary" value="Value" />
-          </div>
-        </Box>
-        <pre class="variant-code">
-          {`
-          interface TextFieldProps extends svelte.JSX.HTMLAttributes<HTMLInputElement> {
-              variant: TextFieldVariant;
-              label: string;
-              value?: string;
-              helperText?: string;
-              color?: ThemeColors;
-              characterCounter?: boolean;
-              error?: boolean;
-          }
-          ___________________________________________
-          
-          <TextField 
-            required 
-            label="Label" 
-            variant="outlined" 
-            color="primary" 
-          />
-          <TextField
-            label="Label"
-            variant="outlined"
-            color="primary"
-            helperText="supporting text"
-          />
-          <TextField
-            label="Label"
-            variant="outlined"
-            color="primary"
-            characterCounter={true}
-            maxlength={60}
-          />
-          <TextField 
-            disabled 
-            autofocus
-            label="Label" 
-            variant="outlined" 
-            color="primary" 
-          />
-          <TextField 
-            error 
-            label="Label" 
-            variant="outlined" 
-            color="primary" 
-            value="Value" 
-          />
-          `}
-        </pre>
+        <PropsExample />
+        <VariantExample code={PROPS_EXP} />
       </Box>
     </Accordion>
     <Accordion isOpen={true}>
       <Typography slot="headerContent" variant="h6">Slots</Typography>
       <Box slot="content">
-        <Typography variant="body1"
-          >Text Field accepts two slots: leadingIcon and trailingIcon. If you're unfamiliar with the
+        <Typography variant="body1">
+          Text Field accepts two slots: leadingIcon and trailingIcon. If you're unfamiliar with the
           concept of slots, think of them like children. To pass a Text Field an icon, use the
           TextFieldIcon component. Make sure to declare the variant ('leading' or 'trailing') as
-          well as the slot name ('leadingIcon' or 'trailingIcon')</Typography
-        >
-        <Box class="text-field-example">
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Leading Icon:</Typography>
-            <TextField label="Label" variant="outlined" color="primary">
-              <TextFieldIcon variant="leading" slot="leadingIcon" class="material-symbols-outlined">
-                search
-              </TextFieldIcon>
-            </TextField>
-          </div>
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Trailing Icon:</Typography>
-            <TextField label="Label" variant="outlined" color="primary">
-              <TextFieldIcon
-                variant="trailing"
-                slot="trailingIcon"
-                class="material-symbols-outlined"
-              >
-                close
-              </TextFieldIcon>
-            </TextField>
-          </div>
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Both Icons:</Typography>
-            <TextField label="Label" variant="outlined" color="primary">
-              <TextFieldIcon variant="leading" slot="leadingIcon" class="material-symbols-outlined">
-                search
-              </TextFieldIcon>
-              <TextFieldIcon
-                variant="trailing"
-                slot="trailingIcon"
-                class="material-symbols-outlined"
-              >
-                close
-              </TextFieldIcon>
-            </TextField>
-          </div>
-        </Box>
-        <pre class="variant-code">
-          {`
-          <TextField
-            label="my label"
-            variant="filled"
-            color="primary"
-          >
-            <TextFieldIcon 
-              variant="leading" 
-              slot="leadingIcon" 
-              class="material-symbols-outlined"
-            >
-              search
-            </TextFieldIcon>
-          </TextField>
-          <TextField
-            label="my label"
-            variant="filled"
-            color="primary"
-          >
-            <TextFieldIcon 
-              variant="trailing" 
-              slot="trailingIcon" 
-              class="material-symbols-outlined"
-            >
-              close
-            </TextFieldIcon>
-          </TextField>
-          <TextField
-            label="my label"
-            variant="filled"
-            color="primary"
-          >
-            <TextFieldIcon 
-              variant="leading" 
-              slot="leadingIcon" 
-              class="material-symbols-outlined"
-            >
-              search
-            </TextFieldIcon>
-            <TextFieldIcon 
-              variant="trailing" 
-              slot="trailingIcon" 
-              class="material-symbols-outlined"
-            >
-              close
-            </TextFieldIcon>
-          </TextField>
-          `}
-        </pre>
+          well as the slot name ('leadingIcon' or 'trailingIcon')
+        </Typography>
+
+        <SlotsExample />
+        <VariantExample code={SLOTS_EXP} />
       </Box>
     </Accordion>
+
     <Accordion isOpen={true}>
       <Typography slot="headerContent" variant="h6">Validation</Typography>
       <Box slot="content">
@@ -223,47 +61,12 @@
           the value, making this a controlled input. You can still use the Text Field as an
           unctrolled input and the value will be captured by the form.
         </Typography>
-        <Box class="text-field-example">
-          <form
-            on:submit={() => {
-              alert(`you submitted ${formValue}`);
-              formValue = '';
-            }}
-          >
-            <div class="labeled-example">
-              <Typography variant="subtitle1">Validate:</Typography>
-              <TextField
-                bind:value={formValue}
-                inputProps={{ required: true, pattern: '.+@.+.com', maxlength: 20, minlength: 8 }}
-                variant="outlined"
-                label="Email"
-                helperText="Email format"
-                characterCounter
-              />
-            </div>
-            <Button style={'margin-top: 2px;'} type="submit" variant="text">Submit</Button>
-          </form>
-        </Box>
-        <pre class="variant-code">
-          {`
-           <form on:submit={() => alert(\`you submitted ${formValue}\`)}>
-            <TextField
-              required
-              bind:value={formValue}
-              variant="outlined"
-              label="Email"
-              pattern=".+@.+\\.com"
-              helperText="Email format"
-              characterCounter={true}
-              maxlength={20}
-              minlength={8}
-            />
-            <Button style={'margin-top: 2px;'} type="submit" variant="text">Submit</Button>
-          </form>
-            `}
-        </pre>
+
+        <ValidationExample />
+        <VariantExample code={VALIDATION_EXP} />
       </Box>
     </Accordion>
+
     <Accordion isOpen={true}>
       <Typography slot="headerContent" variant="h6">Theming</Typography>
       <Box slot="content">
@@ -275,79 +78,9 @@
           default, use --on-background for their color. You can override that color by adding a
           style tag to your TextFieldIcon with a "!important".
         </Typography>
-        <Box class="text-field-example">
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Override Focus:</Typography>
-            <TextField
-              variant="outlined"
-              label="Label"
-              color="secondary"
-              value="Value"
-              helperText="Click me: color is secondary"
-            />
-          </div>
-          <div class="labeled-example">
-            <Typography variant="subtitle1">Override Icon:</Typography>
-            <TextField
-              label="Label"
-              variant="outlined"
-              color="primary"
-              helperText="icon color is overridden by custom style prop"
-            >
-              <TextFieldIcon
-                variant="leading"
-                slot="leadingIcon"
-                class="material-symbols-outlined"
-                style="color: pink !important;"
-              >
-                search
-              </TextFieldIcon>
-            </TextField>
-          </div>
-        </Box>
-        <pre class="variant-code">
-          {`
-            Props: color, onSurfaceColor
 
-            Theming (optional, overrides the default color):
-                Palette: {
-                  textField: {
-                    // "outlined" border, label, helper text, and character counter colors
-                    // "filled" bottom line, helper text, and character counter colors
-                    inactive: 'value',
-                    // "outlined" border color
-                    hover: 'value',
-                    // "filled" background and bottom line
-                    background: 'value',
-                    backgroundHover: 'value',
-                    bottomLineHover: 'value',
-                  }
-                }
-          ____________________________________________________________________
-
-          <TextField
-            variant="outlined"
-            label="Label"
-            color="secondary"
-            helperText="color is secondary"
-          />
-          <TextField
-            label="Label"
-            variant="outlined"
-            color="primary"
-            helperText="icon color is overridden by custom style prop"
-          >
-            <TextFieldIcon
-              variant="leading"
-              slot="leadingIcon"
-              class="material-symbols-outlined"
-              style="color: pink !important;"
-            >
-              search
-            </TextFieldIcon>
-          </TextField>
-        `}
-        </pre>
+        <ThemingExample />
+        <VariantExample code={THEMING_EXP} />
       </Box>
     </Accordion>
   </AccordionGroup>
