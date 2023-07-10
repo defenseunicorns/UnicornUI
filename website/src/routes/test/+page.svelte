0e5186148f35a9733dc8a87526d210ac69a4c446
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-07-10 09:45:36 -0600

    #227 -- fixed applying transition to custom component by binding ref of Box in test +page.svelte.

--- website/src/routes/test/+page.svelte
+++ website/src/routes/test/+page.svelte
@@ -3,10 +3,11 @@
   import { current_component, onMount } from 'svelte/internal';
   import { fade } from 'svelte/transition';
   import { create_bidirectional_transition } from 'svelte/internal';
-
   let boxRef: HTMLElement;
 </script>
 
-<Box element="div" transition:fade eventComponent={current_component}>TeSTING</Box>
+<Box element="div" bind:ref={boxRef} transition:fade eventComponent={current_component}>
+  TeSTING
+</Box>
 
-<div bind:this={boxRef}>Element Test</div>
+<div transition:fade>Element Test</div>
