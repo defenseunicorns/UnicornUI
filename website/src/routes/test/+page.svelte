b938b0144876c521fcf947010488d857c5d13031
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-07-10 09:31:35 -0600

    #227 -- updated svelte.config.js transitionPreprocessor() to write in create_bidirectional_transition() in an onMount() -- right now it is working for the div in test +page.svelte but not for <Box>.

--- website/src/routes/test/+page.svelte
+++ website/src/routes/test/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { Box } from '@uui';
+  import { current_component, onMount } from 'svelte/internal';
   import { fade } from 'svelte/transition';
+  import { create_bidirectional_transition } from 'svelte/internal';
+
+  let boxRef: HTMLElement;
 </script>
 
-<Box transition:fade>TeSTING</Box>
+<Box element="div" transition:fade eventComponent={current_component}>TeSTING</Box>
 
-<style>
-</style>
+<div bind:this={boxRef}>Element Test</div>
--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-
 const transitionPreprocessor = {
   markup({ content, filename }) {
-    console.log('transition pre-processing ', filename)
+    console.log('transition pre-processing ', filename);
     let result = content;
     const compOpeningTag = getInlineCompOpenTag(content);
 
@@ -13,11 +12,12 @@ const transitionPreprocessor = {
       if (transPos > -1) {
         const transition = getTransition(compOpeningTag, transPos);
         if (transition) result = removeTransFromCompTag(content, transition);
-        console.log('***** FILE: ', filename, '\n', transition, "\n", result)
+        result = writeToScript(result, transition);
+        console.log('***** FILE: ', filename, '\n', transition, '\n', result);
       }
     }
 
-    return {code: result}
+    return { code: result };
   }
 };
 
@@ -25,7 +25,8 @@ const transitionPreprocessor = {
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [transitionPreprocessor,
+  preprocess: [
+    transitionPreprocessor,
     preprocess({
       scss: {
         includePaths: ['../node_modules']
@@ -43,15 +44,28 @@ const config = {
 
 export default config;
 
+function writeToScript(content, transition) {
+  const end_script_tag = content.search(/<\/script>/);
+  const transFunc = `\n onMount(() => {
+  const trans = boxRef && create_bidirectional_transition(boxRef, ${
+    transition.split(':')[1]
+  }, {}, true);
+  trans.run(1);
+}); \n`;
+  const newContent =
+    content.slice(0, end_script_tag - 1) + transFunc + content.slice(end_script_tag);
+  return newContent;
+}
+
 function getInlineCompOpenTag(content) {
   const tagOpenPos = content.search(/<[A-Z]/);
   const tagClosePos = content.substring(tagOpenPos).search(/>/);
   return content.substring(tagOpenPos, tagOpenPos + tagClosePos + 1);
-};
+}
 
 function getTransPos(content) {
   return content.search(/transition:/);
-};
+}
 
 function getTransParams(content, startSearchInd) {
   const hasParamsPos = content.substring(startSearchInd).search(/={{/);
@@ -62,21 +76,21 @@ function getTransParams(content, startSearchInd) {
         startSearchInd + content.substring(startSearchInd).search(/}}/) + 2
       )
     : '';
-};
+}
 
 function getTransNoParams(content, startSearchInd) {
   return content.substring(
     startSearchInd,
     startSearchInd + content.substring(startSearchInd).search(/\s|>/)
   );
-};
+}
 
 function getTransition(content, transPos) {
   const transParams = getTransParams(content, transPos);
   const transNoParams = getTransNoParams(content, transPos);
   return transParams ? transParams : transNoParams;
-};
+}
 
 function removeTransFromCompTag(content, transition) {
   return content.replace(transition, '');
-};
+}
